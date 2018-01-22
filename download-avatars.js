41e735bf2e8859a7069b0dae90adbf10403d2a78
Konrad <konrad.kipczak@gmail.com>
2018-01-22 19:23:37 +0000

    created module with github token and created function that lists all avatar urls

--- download-avatars.js
+++ download-avatars.js
@@ -1,14 +1,29 @@
 var request = require('request');
+var token = require('./secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, JSON.parse(body));
+
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+    for(var i = 0; i < result.length; i++){
+      console.log('Avatar: ', result[i].avatar_url);
+    }
 });
 
 
