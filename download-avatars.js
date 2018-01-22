c04d511d4c4472744f4de41cc40a3303da64a710
Konrad <konrad.kipczak@gmail.com>
2018-01-22 19:32:39 +0000

    created function that downloads a specific user's avatar to an avatar folder

--- download-avatars.js
+++ download-avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var token = require('./secrets');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -26,4 +27,9 @@ getRepoContributors("jquery", "jquery", function(err, result) {
     }
 });
 
+function downloadImageByURL(url, filePath) {
+  request.get(url).pipe(fs.createWriteStream(filePath));
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "./avatars/kvirani.jpg");
 
