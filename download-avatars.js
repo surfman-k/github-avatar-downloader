e9b8bdc20d651d93c90cd67867c0b072c69a52b9
Konrad <konrad.kipczak@gmail.com>
2018-01-22 21:42:02 +0000

    Cleaned up code

--- download-avatars.js
+++ download-avatars.js
@@ -3,14 +3,15 @@ var token = require('./secrets');
 var fs = require('fs');
 var input = process.argv.slice(2);
 
-
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
+//Checks if arguments were given by user
   if(!repoOwner || !repoName)
     console.log("Please enter both an owner and a name!");
 
+//API request options. Keeps Github Token secret by using module
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -19,15 +20,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+//Requests data from api and parses it
   request(options, function(err, res, body) {
-   cb(err, JSON.parse(body));
+    cb(err, JSON.parse(body));
   });
 }
 
+//Function that will download picture from avatar url and put it into the specified filepath
 function downloadImageByURL(url, filePath) {
   request.get(url).pipe(fs.createWriteStream(filePath));
 }
 
+//Callback function that takes each contributor and finds their avatar url and login info
 function callback(err, result){
    console.log("Errors:", err);
     for(var i = 0; i < result.length; i++){
@@ -35,9 +39,5 @@ function callback(err, result){
     }
 }
 
-
+//main function call that uses command line arguments
 getRepoContributors(input[0], input[1], callback);
-
-
-
-
